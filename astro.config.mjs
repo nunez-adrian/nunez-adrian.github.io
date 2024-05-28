32d3f15b72d4452e95186913ff7f53f0d0056b8d
Adrian Nuñez <nunezruizadrian@gmail.com>
2024-05-28 20:05:12 +0200

    fix: nested css warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import tailwindcssNesting from 'tailwindcss/nesting'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nunez-adrian.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcssNesting()]
+      }
+    }
+  }
 });
